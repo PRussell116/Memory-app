338bc7124108ebb2b1d95cc2b382d65b5bc06e56
PRussell116 <33029749+PRussell116@users.noreply.github.com>
2020-02-17 17:29:04 +0000

    fixed bug with how to play screen

--- app/src/main/java/com/example/memoryapp/howToPlay.java
+++ app/src/main/java/com/example/memoryapp/howToPlay.java
@@ -5,10 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 public class howToPlay extends AppCompatActivity {
 
@@ -16,13 +12,8 @@ public class howToPlay extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_how_to_play);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show());
     }
+
 
     public void goToMainMenu(View view) {
         Intent intent = new Intent(howToPlay.this, StartScreen.class);
